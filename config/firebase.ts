bdda6dc2786df9b07f305ee6a0ae42867f9e3119
Didan Fariz <112478419+didanfarizz@users.noreply.github.com>
2025-08-25 02:43:01 +0700

    .

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import {getDatabase} from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -21,6 +20,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 export const FIREBASE_AUTH = getAuth(app);
 export const db = getDatabase();
